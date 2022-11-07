6c1ce9b6206d2618eb60a7931ee7b0e2da52eef7
selfdevtv <selfdevtv@gmail.com>
2022-11-07 10:22:37 +0000

    Update package-lock.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { Canvas } from '@react-three/fiber';
+import { Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
     <>
       <div>Outside Canvas</div>
       <Canvas>
+        <Sky sunPosition={[10, 10, 10]} />
+        <mesh>
+          <boxGeometry />
+          <meshBasicMaterial />
+        </mesh>
       </Canvas>
     </>
   );
